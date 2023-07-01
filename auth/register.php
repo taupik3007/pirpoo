fa91d05004fbbf5882aac1b8dc08b48a2162fff3
taupik3007 <taupikpathurrohman@gmail.com>
2023-07-01 21:21:09 +0700

    [f-1] [f-2] perbaikan url pada login.php dan register.php

--- auth/register.php
+++ auth/register.php
@@ -8,7 +8,7 @@
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="/public/assets/css/login.css">
+    <link rel="stylesheet" href="../public/assets/css/login.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css" rel="stylesheet" />
 </head>
