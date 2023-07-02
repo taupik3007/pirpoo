eb36d72caee579e60cae93740413fd730609abfe
ParkHyoKyo <budiavizone@gmail.com>
2023-07-02 21:31:49 +0700

    conection

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,11 @@
 <?php
-    $koneksi = mysqly_connect("localhost","root","","pirpoo");
+    $koneksi = mysqli_connect("localhost","root","","pirpoo");
     if ($koneksi == true){
         echo "koneksi berhasil";
     }else{
         echo "koneksi gagal";
     }
-    
+   
 
 
 ?>
