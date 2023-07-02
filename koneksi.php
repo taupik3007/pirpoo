e415b028d71511df184f0567421ab14ec5c15683
ParkHyoKyo <budiavizone@gmail.com>
2023-07-02 21:27:56 +0700

    buat koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
     $koneksi = mysqly_connect("localhost","root","","pirpoo");
-    if ($koneksi == true);{
+    if ($koneksi == true){
         echo "koneksi berhasil";
     }else{
         echo "koneksi gagal";
