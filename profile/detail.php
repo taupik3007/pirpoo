5ce10e64a0d43d08cb4f9645b6517a5c85800459
fahru-razi <henteu.aya.namian@gmail.com>
2023-07-09 23:59:22 +0700

    f-5 penambahan detail profile (10%)

---
+++ profile/detail.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user_id=1</title>
+    <link rel="icon" type="image/png" href="../public/assets/img/Logo pirpo.PNG">
+    <link rel="stylesheet" href="../public/assets/css/profile.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/assets/css/login.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-nav">
+        <div class="container d-flex min-h-screen">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav flex-column">
+                    <li class="nav-item">
+                        <div style="display: flex;">
+                            <a class="navbar-brand" href="../Homepage.php">
+                                <svg width="45" height="45" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd"
+                                        d="M15.545 4.855a.6.6 0 0 1 0 .85L8.77 12.48l6.776 6.775a.6.6 0 1 1-.85.85l-7.2-7.2a.6.6 0 0 1 0-.85l7.2-7.2a.6.6 0 0 1 .85 0Z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                            <div style="center">
+                                <img src="../public/assets/img/pp.png" alt="profile picture"
+                                    style="max-width: 73%; height: auto;">
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item text-center mt-4">
+                        <h3>Username</h3>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-flex justify-content-between align-items-center mt-3 px-4">
+                            <div class="stats">
+                                <h6 class="mb-0">Postingan</h6>
+                                <span>42</span>
+                            </div>
+                            <div class="stats">
+                                <h6 class="mb-0">Pengikut</h6>
+                                <span>1M</span>
+                            </div>
+                            <div class="stats">
+                                <h6 class="mb-0">Mengikuti</h6>
+                                <span>129</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-flex bg-secondary">
+                            <h4>BIO</h4>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
