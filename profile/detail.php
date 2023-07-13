d165b7b7a0e64da840e4acfdd16a890f3c92fb37
fahru-razi <henteu.aya.namian@gmail.com>
2023-07-13 22:37:04 +0700

    create f-5

--- profile/detail.php
+++ profile/detail.php
@@ -1,78 +1,189 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>user_id=1</title>
     <link rel="icon" type="image/png" href="../public/assets/img/Logo pirpo.PNG">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../public/assets/css/profile.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/assets/css/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@600;700;800&display=swap"
+        rel="stylesheet">
 </head>
 
-<body>
+<body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
+    <div class="w-full flex flex-row flex-wrap">
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-nav">
-        <div class="container d-flex min-h-screen">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav flex-column">
-                    <li class="nav-item">
-                        <div style="display: flex;">
-                            <a class="navbar-brand" href="../Homepage.php">
-                                <svg width="45" height="45" fill="currentColor" viewBox="0 0 24 24">
-                                    <path fill-rule="evenodd"
-                                        d="M15.545 4.855a.6.6 0 0 1 0 .85L8.77 12.48l6.776 6.775a.6.6 0 1 1-.85.85l-7.2-7.2a.6.6 0 0 1 0-.85l7.2-7.2a.6.6 0 0 1 .85 0Z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                            </a>
-                            <div style="center">
-                                <img src="../public/assets/img/pp.png" alt="profile picture"
-                                    style="max-width: 73%; height: auto;">
+        <div class="w-full bg-white h-screen flex flex-row flex-wrap justify-center">
+            <!-- Begin Navbar -->
+
+            <!-- Navbar toggle button -->
+            <div
+                class="bg-white shadow-lg border-t-4 border-indigo-500 absolute bottom-0 w-full md:w-0 md:hidden flex flex-row flex-wrap">
+                <div class="w-full text-right">
+                    <button class="p-2 fa fa-bars text-4xl text-gray-600"></button>
+                </div>
+            </div>
+
+            <!-- Sidebar -->
+            <div class="w-0 md:w-1/4 lg:w-1/5 h-0 md:h-screen overflow-y-hidden bg-white border-2">
+                <!-- Profile picture and name -->
+                <div class="p-5 bg-white sticky top-0">
+                    <div class="flex justify-center items-center">
+                        <img class="w-1/2 rounded-full shadow-lg justify-center" src="../public/assets/img/pp.png"
+                            alt="Profile Picture">
+                    </div>
+                    <div class="pt-2 mt-4 w-full text-center text-xl text-black-600">
+                        Some Person
+                    </div>
+                </div>
+                <div class="w-full">
+                    <div class="flex justify-between items-center py-3 px-6">
+                        <div class="text-black-600">
+                            <span class="font-bold">123</span>
+                            <h6 class="text-sm">Postingan</h6>
+                        </div>
+                        <div class="text-black-600">
+                            <span class="font-bold">456</span>
+                            <h6 class="text-sm">Pengikut</h6>
+                        </div>
+                        <div class="text-black-600">
+                            <span class="font-bold">789</span>
+                            <h6 class="text-sm">Mengikuti</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-70 p-4 flex justify-center items-center mx-auto">
+                    <div>
+                        <p class="text-gray-600 text-lg text-center">Bio:</p>
+                        <p class="text-gray-600 text-sm pl-3 pr-2 text-justify">Ini adalah bio pengguna Some Person.
+                            Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit. Sed sagittis condimentum mauris, eu iaculis quam feugiat et. Vestibulum
+                            ante ipsum primis
+                            in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+                    </div>
+                </div>
+
+
+                <!-- Navigation menu -->
+                <div class="w-full h-screen antialiased flex flex-col hover:cursor-pointer mt-8">
+                    <a class="hover:bg-light-300 bg-light-200 p-2 w-full text-base text-left text-black-600 font-semibold"
+                        href="">
+                        <i class="iset pl-3 pr-2 float-left"></i>
+                        Settings
+                    </a>
+                    <a class="hover:bg-light-300 bg-light-200 p-2 w-full text-base text-left text-black-600 font-semibold"
+                        href="">
+                        <i class="ihelp pl-3 pr-2 float-left"></i>
+                        Help
+                    </a>
+                    <a href="../index.php" class="btn-link p-4 text-center text-xl">
+                        <button class="big-btn">Log out</button>
+                    </a>
+                </div>
+            </div>
+
+            <!-- End Navbar -->
+
+            <!-- Main Content -->
+            <div class="w-full md:w-3/4 lg:w-4/5 p-5 md:px-12 lg:24 h-full overflow-x-scroll antialiased">
+                <!-- Create new post -->
+                <div class="bg-white w-full shadow rounded-lg p-5">
+                    <textarea class="bg-gray-200 w-full rounded-lg shadow border p-2" rows="5"
+                        placeholder="Speak your mind"></textarea>
+
+                    <div class="w-full flex flex-row flex-wrap mt-3">
+                        <div class="w-1/3">
+                            <select class="w-full p-2 rounded-lg bg-gray-200 shadow border float-left">
+                                <option>Public</option>
+                                <option>Private</option>
+                            </select>
+                        </div>
+                        <div class="w-2/3">
+                            <button type="button"
+                                class="float-right bg-indigo-400 hover:bg-indigo-300 text-white p-2 rounded-lg">Submit</button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Posts -->
+                <div class="mt-3 flex flex-col">
+                    <!-- Post 1 -->
+                    <div class="bg-white mt-3">
+                        <img class="border rounded-t-lg shadow-lg"
+                            src="https://images.unsplash.com/photo-1572817519612-d8fadd929b00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+                        <div class="bg-white border shadow p-5 text-xl text-gray-700 font-semibold">
+                            A Pretty Cool photo from the mountains.
+                        </div>
+                        <div class="bg-white p-1 border shadow flex flex-row flex-wrap">
+                            <div class="w-1/3 hover:bg-gray-200 text-center text-xl text-gray-700 font-semibold">Like
+                            </div>
+                            <div
+                                class="w-1/3 hover:bg-gray-200 border-l-4 border-r- text-center text-xl text-gray-700 font-semibold">
+                                Share</div>
+                            <div
+                                class="w-1/3 hover:bg-gray-200 border-l-4 text-center text-xl text-gray-700 font-semibold">
+                                Comment
                             </div>
                         </div>
-                    </li>
-                    <li class="nav-item text-center mt-4">
-                        <h3>Username</h3>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex justify-content-between align-items-center mt-3 px-4">
-                            <div class="stats">
-                                <h6 class="mb-0">Postingan</h6>
-                                <span>42</span>
-                            </div>
-                            <div class="stats">
-                                <h6 class="mb-0">Pengikut</h6>
-                                <span>1M</span>
-                            </div>
-                            <div class="stats">
-                                <h6 class="mb-0">Mengikuti</h6>
-                                <span>129</span>
+                        <div
+                            class="bg-white border-4 bg-gray-300 border-white rounded-b-lg shadow p-5 text-xl text-gray-700 content-center font-semibold flex flex-row flex-wrap">
+                            <div class="w-full">
+                                <div class="w-full text-left text-xl text-gray-600">
+                                    @Some Person
+                                </div>
+                                A Pretty Cool photo from the mountains.
+                                A Pretty Cool photo from the mountains.
                             </div>
                         </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex bg-secondary">
-                            <h4>BIO</h4>
+                    </div>
+
+                    <!-- Post 2 -->
+                    <div class="bg-white mt-3">
+                        <img class="border rounded-t-lg shadow-lg"
+                            src="https://images.unsplash.com/photo-1572817519612-d8fadd929b00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+                        <div class="bg-white border shadow p-5 text-xl text-gray-700 font-semibold">
+                            A Pretty Cool photo from the mountains.
                         </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                </ul>
+                        <div class="bg-white p-1 rounded-b-lg border shadow flex flex-row flex-wrap">
+                            <div class="w-1/3 hover:bg-gray-200 text-center text-xl text-gray-700 font-semibold">Like
+                            </div>
+                            <div
+                                class="w-1/3 hover:bg-gray-200 border-l-4 border-r- text-center text-xl text-gray-700 font-semibold">
+                                Share</div>
+                            <div
+                                class="w-1/3 hover:bg-gray-200 border-l-4 text-center text-xl text-gray-700 font-semibold">
+                                Comment
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Post 3 -->
+                    <div class="bg-white mt-3">
+                        <img class="border rounded-t-lg shadow-lg"
+                            src="https://images.unsplash.com/photo-1572817519612-d8fadd929b00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
+                        <div class="bg-white border shadow p-5 text-xl text-gray-700 font-semibold">
+                            A Pretty Cool photo from the mountains.
+                        </div>
+                        <div class="bg-white p-1 rounded-b-lg border shadow flex flex-row flex-wrap">
+                            <div class="w-1/3 hover:bg-gray-200 text-center text-xl text-gray-700 font-semibold">Like
+                            </div>
+                            <div
+                                class="w-1/3 hover:bg-gray-200 border-l-4 border-r- text-center text-xl text-gray-700 font-semibold">
+                                Share</div>
+                            <div
+                                class="w-1/3 hover:bg-gray-200 border-l-4 text-center text-xl text-gray-700 font-semibold">
+                                Comment
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-    </script>
+    </div>
 </body>
 
 </html>
