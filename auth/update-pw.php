cc95dc63111707bc7fa4804d899ce4d68b9c8767
taupik3007 <taupikpathurrohman@gmail.com>
2023-07-16 22:15:29 +0700

    commit bug

--- auth/update-pw.php
+++ auth/update-pw.php
@@ -1,7 +1,7 @@
 <?php
 
 include '../koneksi.php';
-echo $id = $_POST['id'];
+$id = $_POST['id'];
 $password = md5($_POST['password_new']);
 
 $data = mysqli_query($koneksi,"UPDATE `user` SET `password`='$password' WHERE `user_id` = '$id'");
